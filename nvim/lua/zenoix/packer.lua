45e639f7e15147da8a273f84050c82a4e45e3936
Zenoix <41812358+Zenoix@users.noreply.github.com>
2023-06-30 15:50:04 +1200

    Add neovim and tmux configs

---
+++ nvim/lua/zenoix/packer.lua
@@ -0,0 +1,63 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd.packadd('packer.nvim')
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    use ({
+        'folke/tokyonight.nvim',
+        as = 'tokyonight',
+        priority = 1000,
+        config = function()
+            vim.cmd('colorscheme tokyonight')
+        end
+    })
+
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+    use("tpope/vim-fugitive")
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+            'williamboman/mason.nvim',
+            run = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end,
+        },
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},     -- Required
+        {'hrsh7th/cmp-nvim-lsp'}, -- Required
+        {'L3MON4D3/LuaSnip'},     -- Required
+        {'jalvesaq/cmp-nvim-r'}
+    }
+}
+
+use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+}
+
+    use("yuttie/comfortable-motion.vim")
+
+    use {"Vigemus/iron.nvim"}
+
+    use {"jalvesaq/Nvim-R"}
+
+    use {'christoomey/vim-tmux-navigator'}
+end)
