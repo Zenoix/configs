9eb9b8d3931cc5d36cfb4ef05fc849777df303c4
Zenoix <41812358+Zenoix@users.noreply.github.com>
2023-07-25 15:03:43 +1200

    Change nvim-r assignment shortcut

--- nvim/after/plugin/nvim-r.lua
+++ nvim/after/plugin/nvim-r.lua
@@ -4,3 +4,5 @@ vim.cmd('let R_auto_start = 2')
 -- Have more syntax highlighting in the R console
 vim.cmd('let Rout_more_colors = 1')
 
+-- Disable _ turning into arrows 
+vim.cmd('let R_assign = 0')
